1eb4425fe1c5bcdd13025923550af0a3bb5edf6d
Nico? <84991759+NiccoDorn@users.noreply.github.com>
2025-06-08 17:32:32 +0200

    state machine pattern now for better understanding

--- src/main/scala/bitmanipulation/Rotater.scala
+++ src/main/scala/bitmanipulation/Rotater.scala
@@ -45,42 +45,68 @@ class SequentialRotater(bitWidth: Int, generator: () => AbstractFixedRotater)
   // for a shift amount of times, making this algorithm linear time
   // like this we can simply use the 1-bit rotator, apply it n times
   // and count {cnt} how often we used it.
-  // also we need to account for immediate and sequential cases still...
-
-  val rotate = RegInit(false.B)
+  // We use a state machine approach to handle immediate, idle and rotation cases
+  // in the sequential rotater
+  
+  val sIdle :: sRotate :: sDone :: Nil = Enum(3) // good old ocaml list states hehe
+  val state = RegInit(sIdle)
+  
   val cnt = RegInit(0.U(log2Ceil(bitWidth).W))
   val shamtT = RegInit(0.U(log2Ceil(bitWidth).W))
   val currVal = RegInit(0.U(bitWidth.W))
   val resultReg = RegInit(0.U(bitWidth.W))
   
-  // weird handling of immediate and sequential cases
-  // for io.done and io.result to satisfy weird tests
-  io.done := (!rotate && io.start && io.shamt === 0.U) || (rotate && cnt + 1.U === shamtT) || (!rotate && !io.start && resultReg =/= 0.U)
-  io.result := Mux(!rotate && io.start && io.shamt === 0.U, io.input,
-                  Mux(rotate && cnt + 1.U === shamtT, Rotater.io.result, 
-                    Mux(!rotate && !io.start, resultReg, currVal)))
-  // now the timing to present the values should be right.
+  val doneTemp = WireDefault(false.B)
+  val resultTemp = WireDefault(0.U(bitWidth.W))
   
+  switch(state) {
+    is(sIdle) {
+      doneTemp := io.start && io.shamt === 0.U  // immediate
+      resultTemp := Mux(io.start && io.shamt === 0.U, io.input, 0.U)
+    }
+    is(sRotate) {
+      doneTemp := cnt + 1.U === shamtT  // just finished, provide done and result
+      resultTemp := Mux(cnt + 1.U === shamtT, Rotater.io.result, currVal)
+    }
+    is(sDone) {
+      doneTemp := true.B
+      resultTemp := resultReg
+    }
+  }
+  io.done := doneTemp
+  io.result := resultTemp
+  
+  // actual rotation
   Rotater.io.input := currVal
-  
-  when(!rotate) {
-    when(io.start) {
-      when(io.shamt === 0.U) {
-        currVal := io.input
-      }.otherwise {
-        currVal := io.input
-        shamtT := io.shamt
-        cnt := 0.U
-        rotate := true.B
+  switch(state) {
+    is(sIdle) {
+      when(io.start) {
+        when(io.shamt === 0.U) {
+          // no shifting needed, immediately assign currVal the provided input
+          currVal := io.input
+        }.otherwise {
+          // rotation case with shamt > 0
+          currVal := io.input
+          shamtT := io.shamt
+          cnt := 0.U
+          state := sRotate
+        }
       }
     }
-  }.otherwise {
-    currVal := Rotater.io.result
-    cnt := cnt + 1.U
-
-    when(cnt + 1.U === shamtT) { // done, rotated cnt = 0 ... n-1 = n times
-      rotate := false.B
-      resultReg := Rotater.io.result  // oops, not forgetting to store final result
+    
+    is(sRotate) {
+      currVal := Rotater.io.result // only now pass to fixed rotater
+      cnt := cnt + 1.U
+      when(cnt + 1.U === shamtT) { // check if already finished
+        resultReg := Rotater.io.result // overwrite io.result as soon as possible
+        state := sDone  // same goes for state
+      }
+    }
+    
+    is(sDone) {
+      when(!io.start) {
+        state := sIdle
+      }
     }
   }
 }
